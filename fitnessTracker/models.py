746ef6b2da0fd6ffa3c809e5dfa3aefa8bad0598
H Arjun <harjun2090@gmail.com>
2017-05-28 17:17:14 +0530

    index page, measurement table, applied migrations

--- fitnessTracker/models.py
+++ fitnessTracker/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Measurements(models.Model):
+    weight = models.FloatField() # in kg
+    waist  = models.FloatField() # in inches
--- fitness_tracker/settings.py
+++ fitness_tracker/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'fitnessTracker.apps.FitnesstrackerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,10 @@ WSGI_APPLICATION = 'fitness_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fitness_tracker',
+        'USER': 'arjun',
+        'PASSWORD': '',
     }
 }
 
