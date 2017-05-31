365172a8b761ced7be9289b9a69bcb1783060ee1
H Arjun <harjun2090@gmail.com>
2017-05-31 23:08:02 +0530

    schema updates

--- fitnessTracker/models.py
+++ fitnessTracker/models.py
@@ -1,6 +1,28 @@
 from django.db import models
 
-# Create your models here.
+# measurement track database
 class Measurements(models.Model):
+    timestamp = models.IntegerField() # unix timestamp
     weight = models.FloatField() # in kg
     waist  = models.FloatField() # in inches
+
+# food database
+class Foods(models.Model):
+    name = models.CharField(max_length=100)
+    alternateNames = models.CharField(max_length=200)
+    #foodType = models.CharField()
+    calories = models.FloatField()
+    protein = models.FloatField()
+    carbohydrates = models.FloatField()
+    fat = models.FloatField()
+
+# exercise database   
+class Exercises(models.Model):
+    name = models.CharField(max_length=100)
+    #todo
+
+# workout track database    
+# TODO
+
+# diet track database
+# TODO
